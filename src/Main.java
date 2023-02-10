51b9bf8cf705ce05049ee59b50da14141d060f4a
kanykei063129 <akjoltoikyzykanykei@gmail.com>
2023-02-10 17:37:37 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,9 @@ import java.util.concurrent.Callable;
 
 public class Main {
     public static void main(String[] args) {
-        Vehicle vehicle = new Vehicle("BMW");
-        Car car = new Car(vehicle.name, "Black",312);
-        System.out.println(car);
-
-        car.carSpead();
-        car.price();
-        vehicle.myMethod();
+        Banana banana = new Banana("yellow","B6 C",1);
+        Apple apple = new Apple("red",2,banana);
+        Orange orange = new Orange("Orange",3,banana);
+        System.out.println(apple+"\n"+orange+"\n"+banana);
     }
 }
